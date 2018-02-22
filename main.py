a16ae46a62e2314ef95cb8c76c5ad89d1269155e
h404z <storyabouthoz@163.com>
2018-02-22 23:50:06 +0800

    add: 好友签名特征分析的函数声明

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 # @Author: h404z
 
 import itchat
+import re
+import numpy
+import PIL
+import wordcloud
 
 
 class CYW(object):
@@ -28,6 +32,25 @@ class CYW(object):
         print('女生人数：{}， 占总人数的{:%}%'.format(girls, round(girls / total, 2)))
         print('不明性别人数：{}， 占总人数的{:%}%'.format(other, round(other / total, 2)))
         return {'boys': boys, 'girls': girls, 'other': other}
+
+    def analyse_signature(self):
+        """分析好友签名特征"""
+
+        def clean_signatrure():
+            """
+            很多本来是表情的，变成了emoji、span、class等等这些无关紧要的词，需要先替换掉，
+            另外，还有类似<>/= 之类的符号，也需要写个简单的正则替换掉，再把所有拼起来，得到text字串
+            """
+            pass
+
+        def extract_signature():
+            """用jieba分词提取"""
+            pass
+
+        sig_list = []
+        # todo: 继续完成好友签名分析功能
+
+        pass
 
     pass
 
